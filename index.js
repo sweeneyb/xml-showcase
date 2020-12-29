e132e546073d52da1b2fdb83afed314a75e961bc
sweeneyb <tab126@gmail.com>
2020-12-29 13:36:40 -0500

    add a license and a few endpoints

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var x = require('libxmljs')
+const fs = require('fs')
+
 var express = require('express');
 var app = express();
 var port = 3000;
@@ -9,5 +12,73 @@ app.set('view engine', 'pug');
 app.get('/', function(req, res){
     res.render('index');
 });
+
+app.get('/license/:number', async function (req, res) {
+    try {
+        const license = await getLicnese(req.params.number)
+        res.setHeader('content-type', 'text/xml');
+        res.send(license)
+    }
+    catch (err) {
+        res.status(404)
+        res.send(err)
+    }
+})
+
+function getLicnese(number) {
+    return new Promise( (resolve, reject) => {
+        if(number == "5555") {
+            try {
+                fs.readFile('public/license.xml', 'utf-8', (err, data) => { 
+                    if (err) throw err; 
+                    console.log(data); 
+                    resolve(data)
+                })
+            }
+            catch (error) {
+                console.log(error)
+                reject(error)
+            }
+        } else {
+            reject ("License not found.")
+        }
+    })
+}
+
+app.get('/quote/:sic', function (req, res) {
+    getQuoteForSic(req.params.sic)
+    .then(result => {
+        res.setHeader('content-type', 'text/xml');
+        res.send(result)
+    })
+    .catch(error => { 
+        res.status(404)
+        res.send(error)
+    })
+})
+
+function getQuoteForSic(sic) {
+    return new Promise((resolve, reject) => {
+        if(sic == "07839901") {
+            try {
+                fs.readFile('public/basicResponse.xml', 'utf-8', (err, data) => { 
+                    if (err) throw err; 
+                    console.log(data); 
+                    resolve(data)
+                })
+            }
+            catch (error) {
+                console.log(error)
+                reject(error)
+            }
+            
+         } else {
+            reject("We can't insure that SIC code");
+         }
+       
+    });
+  }
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
